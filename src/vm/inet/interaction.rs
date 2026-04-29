e5db70bec1a9f39408e1c442e724ecda487c7f66
ast-ral <ast.hyphen.ral@gmail.com>
2026-04-29 00:56:21 -0700

    implement descend nodes

--- src/vm/inet/interaction.rs
+++ src/vm/inet/interaction.rs
@@ -73,8 +73,43 @@ pub(crate) fn interact(left: &Port, right: &Port) {
 			ascend_ascend(left_level, right_level)
 		},
 
-		(&Data::Ascend { .. }, &Data::Binding { index }) => ascend_binding(index),
-		(&Data::Binding { index }, &Data::Ascend { .. }) => mirror(ascend_binding(index)),
+		(&Data::Ascend { .. }, &Data::Binding { index }) => binding_unaffected(index),
+		(&Data::Binding { index }, &Data::Ascend { .. }) => mirror(binding_unaffected(index)),
+
+		(&Data::Ascend { level: ascend_level }, &Data::Descend { level: descend_level }) => {
+			ascend_descend(ascend_level, descend_level)
+		},
+		(&Data::Descend { level: descend_level }, &Data::Ascend { level: ascend_level }) => {
+			mirror(ascend_descend(ascend_level, descend_level))
+		},
+
+		(&Data::Descend { level }, &Data::Lambda { live: true }) => descend_lambda(level),
+		(&Data::Lambda { live: true }, &Data::Descend { level }) => mirror(descend_lambda(level)),
+
+		(&Data::Descend { level }, &Data::Application { live }) => descend_application(level, live),
+		(&Data::Application { live }, &Data::Descend { level }) => mirror(descend_application(level, live)),
+
+		(
+			&Data::Descend { level: ascend_level },
+			&Data::Replicator {
+				level: replicator_level,
+				count: replicator_count,
+			},
+		) => descend_replicator(ascend_level, replicator_level, replicator_count),
+		(
+			&Data::Replicator {
+				level: replicator_level,
+				count: replicator_count,
+			},
+			&Data::Descend { level: ascend_level },
+		) => mirror(descend_replicator(ascend_level, replicator_level, replicator_count)),
+
+		(&Data::Descend { level: left_level }, &Data::Descend { level: right_level }) => {
+			descend_descend(left_level, right_level)
+		},
+
+		(&Data::Descend { .. }, &Data::Binding { index }) => binding_unaffected(index),
+		(&Data::Binding { index }, &Data::Descend { .. }) => mirror(binding_unaffected(index)),
 
 		(&Data::Reformat, &Data::Lambda { live: true }) => reformat_lambda(),
 		(&Data::Lambda { live: true }, &Data::Reformat) => mirror(reformat_lambda()),
@@ -85,6 +120,9 @@ pub(crate) fn interact(left: &Port, right: &Port) {
 		(&Data::Reformat, &Data::Ascend { level }) => reformat_ascend(level),
 		(&Data::Ascend { level }, &Data::Reformat) => mirror(reformat_ascend(level)),
 
+		(&Data::Reformat, &Data::Descend { level }) => reformat_descend(level),
+		(&Data::Descend { level }, &Data::Reformat) => mirror(reformat_descend(level)),
+
 		(&Data::Reformat, &Data::Reformat) => reformat_reformat(),
 
 		(&Data::Unlink { level }, &Data::Lambda { live: true }) => unlink_lambda(level),
@@ -92,9 +130,6 @@ pub(crate) fn interact(left: &Port, right: &Port) {
 
 		(&Data::Unlink { level }, &Data::Application { live: false }) => unlink_application(level),
 		(&Data::Application { live: false }, &Data::Unlink { level }) => mirror(unlink_application(level)),
-
-		(&Data::Unlink { level }, &Data::Ascend { .. }) => unlink_ascend(level),
-		(&Data::Ascend { .. }, &Data::Unlink { level }) => mirror(unlink_ascend(level)),
 
 		(&Data::Unlink { level }, &Data::Binding { index }) => unlink_binding(level, index),
 		(&Data::Binding { index }, &Data::Unlink { level }) => mirror(unlink_binding(level, index)),
@@ -112,14 +147,14 @@ fn application_lambda() -> (Vec<Port>, Vec<Port>) {
 	let right_anchor_in = anchor();
 	let right_anchor_out = anchor();
 
-	let in_ascend = Node::new(Data::Ascend { level: 0 });
-	let out_ascend = Node::new(Data::Ascend { level: 0 });
+	let ascend = Node::new(Data::Ascend { level: 0 });
+	let descend = Node::new(Data::Descend { level: 0 });
 
-	Port::link(&left_anchor_in, &in_ascend.main());
-	Port::link(&in_ascend.aux(0), &right_anchor_in);
+	Port::link(&left_anchor_in, &ascend.main());
+	Port::link(&ascend.aux(0), &right_anchor_in);
 
-	Port::link(&left_anchor_out, &out_ascend.main());
-	Port::link(&out_ascend.aux(0), &right_anchor_out);
+	Port::link(&left_anchor_out, &descend.aux(0));
+	Port::link(&descend.main(), &right_anchor_out);
 
 	(vec![left_anchor_in, left_anchor_out], vec![
 		right_anchor_in,
@@ -379,14 +414,124 @@ fn ascend_ascend(mut left_level: usize, mut right_level: usize) -> (Vec<Port>, V
 	(vec![left_anchor], vec![right_anchor])
 }
 
-fn ascend_binding(index: usize) -> (Vec<Port>, Vec<Port>) {
+fn ascend_descend(mut ascend_level: usize, mut descend_level: usize) -> (Vec<Port>, Vec<Port>) {
 	let left_anchor = anchor();
+	let right_anchor = anchor();
 
-	let binding = Node::new(Data::Binding { index });
+	match ascend_level.cmp(&descend_level) {
+		Ordering::Equal => panic!(),
+		Ordering::Less => descend_level += 1,
+		Ordering::Greater => ascend_level -= 1,
+	}
 
-	Port::link(&left_anchor, &binding.main());
+	let descend = Node::new(Data::Descend { level: descend_level });
+	let ascend = Node::new(Data::Ascend { level: ascend_level });
 
-	(vec![left_anchor], Vec::new())
+	Port::link(&left_anchor, &descend.main());
+	Port::link(&descend.aux(0), &ascend.aux(0));
+	Port::link(&ascend.main(), &right_anchor);
+
+	(vec![left_anchor], vec![right_anchor])
+}
+
+fn descend_lambda(level: usize) -> (Vec<Port>, Vec<Port>) {
+	let left_anchor = anchor();
+	let right_anchor_in = anchor();
+	let right_anchor_out = anchor();
+
+	let lambda = Node::new(Data::Lambda { live: true });
+	let descend_in = Node::new(Data::Descend { level: level + 1 });
+	let descend_out = Node::new(Data::Descend { level: level + 1 });
+
+	Port::link(&left_anchor, &lambda.main());
+
+	Port::link(&lambda.aux(0), &descend_in.aux(0));
+	Port::link(&descend_in.main(), &right_anchor_in);
+
+	Port::link(&lambda.aux(1), &descend_out.aux(0));
+	Port::link(&descend_out.main(), &right_anchor_out);
+
+	(vec![left_anchor], vec![right_anchor_in, right_anchor_out])
+}
+
+fn descend_application(level: usize, live: bool) -> (Vec<Port>, Vec<Port>) {
+	let left_anchor = anchor();
+	let right_anchor_in = anchor();
+	let right_anchor_out = anchor();
+
+	let application = Node::new(Data::Application { live });
+	let descend_in = Node::new(Data::Descend { level });
+	let descend_out = Node::new(Data::Descend { level });
+
+	Port::link(&left_anchor, &application.main());
+
+	Port::link(&application.aux(0), &descend_in.aux(0));
+	Port::link(&descend_in.main(), &right_anchor_in);
+
+	Port::link(&application.aux(1), &descend_out.aux(0));
+	Port::link(&descend_out.main(), &right_anchor_out);
+
+	(vec![left_anchor], vec![right_anchor_in, right_anchor_out])
+}
+
+fn descend_replicator(
+	descend_level: usize,
+	mut replicator_level: usize,
+	replicator_count: usize,
+) -> (Vec<Port>, Vec<Port>) {
+	debug_assert!(replicator_level != descend_level);
+
+	if replicator_level > descend_level {
+		replicator_level -= 1;
+	}
+
+	let left_anchor = anchor();
+	let mut right_anchors = Vec::new();
+
+	let replicator = Node::new(Data::Replicator {
+		level: replicator_level,
+		count: replicator_count,
+	});
+
+	Port::link(&left_anchor, &replicator.main());
+
+	for i in 0 .. replicator_count {
+		let right_anchor = anchor();
+
+		let ascend = Node::new(Data::Descend { level: descend_level });
+
+		Port::link(&replicator.aux(i), &ascend.aux(0));
+		Port::link(&ascend.main(), &right_anchor);
+		right_anchors.push(right_anchor);
+	}
+
+	(vec![left_anchor], right_anchors)
+}
+
+fn descend_descend(mut left_level: usize, mut right_level: usize) -> (Vec<Port>, Vec<Port>) {
+	let left_anchor = anchor();
+	let right_anchor = anchor();
+
+	match left_level.cmp(&right_level) {
+		Ordering::Equal => {
+			Port::link(&left_anchor, &right_anchor);
+
+			return (vec![left_anchor], vec![right_anchor]);
+		},
+		Ordering::Less => right_level -= 1,
+		Ordering::Greater => left_level -= 1,
+	}
+
+	swap(&mut left_level, &mut right_level);
+
+	let left_ascend = Node::new(Data::Descend { level: left_level });
+	let right_ascend = Node::new(Data::Descend { level: right_level });
+
+	Port::link(&left_anchor, &left_ascend.main());
+	Port::link(&left_ascend.aux(0), &right_ascend.aux(0));
+	Port::link(&right_ascend.main(), &right_anchor);
+
+	(vec![left_anchor], vec![right_anchor])
 }
 
 fn reformat_lambda() -> (Vec<Port>, Vec<Port>) {
@@ -434,6 +579,20 @@ fn reformat_ascend(level: usize) -> (Vec<Port>, Vec<Port>) {
 	let right_anchor = anchor();
 
 	let ascend = Node::new(Data::Ascend { level });
+	let reformat = Node::new(Data::Reformat);
+
+	Port::link(&left_anchor, &ascend.main());
+	Port::link(&ascend.aux(0), &reformat.aux(0));
+	Port::link(&reformat.main(), &right_anchor);
+
+	(vec![left_anchor], vec![right_anchor])
+}
+
+fn reformat_descend(level: usize) -> (Vec<Port>, Vec<Port>) {
+	let left_anchor = anchor();
+	let right_anchor = anchor();
+
+	let ascend = Node::new(Data::Descend { level });
 	let reformat = Node::new(Data::Reformat);
 
 	Port::link(&left_anchor, &ascend.main());
@@ -493,22 +652,20 @@ fn unlink_application(level: usize) -> (Vec<Port>, Vec<Port>) {
 	(vec![left_anchor], vec![right_anchor_in, right_anchor_out])
 }
 
-fn unlink_ascend(level: usize) -> (Vec<Port>, Vec<Port>) {
-	let left_anchor = anchor();
-	let right_anchor = anchor();
-
-	let unlink = Node::new(Data::Unlink { level });
-
-	Port::link(&left_anchor, &unlink.aux(0));
-	Port::link(&unlink.main(), &right_anchor);
-
-	(vec![left_anchor], vec![right_anchor])
-}
-
 fn unlink_binding(level: usize, index: usize) -> (Vec<Port>, Vec<Port>) {
 	let left_anchor = anchor();
 
 	let binding = Node::new(Data::Binding { index: level - index });
+
+	Port::link(&left_anchor, &binding.main());
+
+	(vec![left_anchor], Vec::new())
+}
+
+fn binding_unaffected(index: usize) -> (Vec<Port>, Vec<Port>) {
+	let left_anchor = anchor();
+
+	let binding = Node::new(Data::Binding { index });
 
 	Port::link(&left_anchor, &binding.main());
 
