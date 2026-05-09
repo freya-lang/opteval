34e56bcb58c719148e23534bcb5802aebd865c51
ast-ral <ast.hyphen.ral@gmail.com>
2026-05-09 02:50:02 -0700

    basic implementation of scopes

---
+++ src/vm/oracle/scope.rs
@@ -0,0 +1,47 @@
+use std::cmp::Ordering;
+
+use crate::vm::oracle::order::OrderedElement;
+
+#[derive(Clone)]
+pub(crate) struct Scope {
+	start: OrderedElement,
+	end: OrderedElement,
+}
+
+impl Scope {
+	pub(crate) fn new_root() -> Self {
+		let base = OrderedElement::new_base();
+
+		let start = base.iota();
+		let end = start.iota();
+
+		Self { start, end }
+	}
+
+	pub(crate) fn new_child(&self) -> Self {
+		let start = self.start.iota();
+		let end = start.iota();
+
+		Self { start, end }
+	}
+
+	pub(crate) fn directly_related(&self, other: &Self) -> bool {
+		match self.start.cmp(&other.start) {
+			Ordering::Less => match self.end.cmp(&other.end) {
+				Ordering::Less => false,
+				Ordering::Equal => unreachable!(),
+				Ordering::Greater => true,
+			},
+			Ordering::Equal => {
+				debug_assert!(self.end == other.end);
+
+				true
+			},
+			Ordering::Greater => match self.end.cmp(&other.end) {
+				Ordering::Less => true,
+				Ordering::Equal => unreachable!(),
+				Ordering::Greater => false,
+			},
+		}
+	}
+}
